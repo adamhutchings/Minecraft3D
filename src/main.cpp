a9932ecad67df4a454e9eee379ae66a322ecb7f6
adamhutchings <adam.abahot@gmail.com>
2021-06-16 10:04:53 -0700

    Update view matrix

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 #include <vector>
 
+#include <graphics/camera.hpp>
 #include <graphics/mesh.hpp>
 #include <graphics/shader.hpp>
 #include <graphics/texture.hpp>
@@ -30,6 +31,8 @@ int main() {
 
 	tex::load();
 
+	GAME_SHADER->register_uniform("game_matrix");
+
 	std::vector coords{
 		-0.5f,  0.5f,  0.0f,
 		 0.5f,  0.5f,  0.0f,
@@ -45,7 +48,14 @@ int main() {
 
 	Mesh mesh(coords, indices, texcoords);
 
+	Camera camera{};
+	camera.pitch(30.0f, ViewMode::SET);
+	camera.position(2, 2, 2, ViewMode::SET);
+	camera.yaw(210.0f, ViewMode::SET);
+
 	while (!glfwWindowShouldClose(wn)) {
+		// Update camera position
+		GAME_SHADER->set_uniform_value("game_matrix", camera.get_transform_matrix());
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		mesh.draw();
 		glfwSwapBuffers(wn);
