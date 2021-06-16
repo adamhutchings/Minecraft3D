8d61564ca2678cc97764676c347936a818873f3c
adamhutchings <adam.abahot@gmail.com>
2021-06-16 10:04:42 -0700

    Add camera class and transformations

---
+++ src/graphics/camera.cpp
@@ -0,0 +1,57 @@
+#include "camera.hpp"
+
+Camera::Camera() {
+
+	// Lots of zeros!
+	position(0, 0, 0, SET);
+	yaw     (0, SET);
+	pitch   (0, SET);
+
+	fov      = 70.0f;
+
+	nearclip = 0.01f;
+	farclip  = 100.0f;
+
+}
+
+void Camera::pitch(float angle, ViewMode mode) {
+	if (mode == ADJUST) {
+		pitch_ += angle;
+	} else if (mode == SET) {
+		pitch_  = angle;
+	}
+}
+
+void Camera::yaw(float angle, ViewMode mode) {
+	if (mode == ADJUST) {
+		yaw_ += angle;
+	} else if (mode == SET) {
+		yaw_  = angle;
+	}
+}
+
+void Camera::position(float x, float y, float z, ViewMode mode) {
+	if (mode == ADJUST) {
+		location.x += x;
+		location.y += y;
+		location.z += z;
+	} else if (mode == SET) {
+		location.x  = x;
+		location.y  = y;
+		location.z  = z;
+	}
+}
+
+glm::mat4 Camera::get_transform_matrix() {
+
+	auto proj = glm::perspective(glm::radians(fov), 1.0f, nearclip, farclip);
+
+    glm::vec3 direction;
+    direction.x =  cos(glm::radians(pitch_)) * cos(glm::radians(yaw_));
+    direction.y = -sin(glm::radians(pitch_));
+    direction.z =  cos(glm::radians(pitch_)) * sin(glm::radians(yaw_));
+    auto view   = glm::lookAt(location, location + glm::normalize(direction), glm::vec3(0, 1, 0));
+
+    return proj * view;
+
+}
---
+++ src/graphics/camera.hpp
@@ -0,0 +1,31 @@
+/**
+ * Camera - adjust the viewing angle which
+ * in-game objects are rendered from.
+ */
+
+#pragma once
+
+#include <glm/gtc/matrix_transform.hpp>
+
+enum ViewMode {
+	SET,
+	ADJUST,
+};
+
+class Camera {
+
+private:
+	float pitch_, yaw_, fov, nearclip, farclip;
+	glm::vec3 location;
+
+public:
+	Camera();
+	// Change the pitch
+	void pitch(float angle, ViewMode mode);
+	// Change the yaw
+	void yaw  (float angle, ViewMode mode);
+	void position(float x, float y, float z, ViewMode mode);
+	// Multiply this in the fragment shader to make a view.
+	glm::mat4 get_transform_matrix();
+
+};
