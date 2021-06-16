636cef9d5419137ea43911409990d59dd1716941
adamhutchings <adam.abahot@gmail.com>
2021-06-16 10:05:04 -0700

    Use view matrix for coords

--- src/shader/game_vert.glsl
+++ src/shader/game_vert.glsl
@@ -3,9 +3,11 @@
 layout(location=0) in vec3   inVertexCoord;
 layout(location=1) in vec2   inTexCoord;
 
+uniform mat4 game_matrix;
+
 out vec2 texCoord;
 
 void main() {
-    gl_Position = vec4(inVertexCoord, 1.0);
+    gl_Position = game_matrix * vec4(inVertexCoord, 1.0);
     texCoord = inTexCoord;
 }
