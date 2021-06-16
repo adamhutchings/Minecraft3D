cd8c3e5bdfb40168531a47eace2479ceb21f1d66
adamhutchings <adam.abahot@gmail.com>
2021-06-16 15:45:56 -0700

    Add chunk interface

--- src/world/chunk.hpp
+++ src/world/chunk.hpp
@@ -4,6 +4,26 @@
 
 #pragma once
 
+#include <array>
+#include <memory>
+
+#include <graphics/mesh.hpp>
+#include <world/blocks.hpp>
+
 class Chunk {
+
+private:
+	static const int CHUNK_SIZE = 16;
+	// All of the blocks in the mesh
+	std::array<BlockType, CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE> blocks;
+	// The mesh of all of the blocks
+	std::unique_ptr<Mesh> chunk_mesh;
+	bool block_borders_air(int i, int j, int k, Side side);
+
+public:
+	Chunk();
+	void update_mesh(); // only do this when the blocks in the chunk are changed
+	BlockType& at(int x, int y, int z);
+	void render();
 
 };
