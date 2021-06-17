a6f81d0ba52687a72f2ea54d39bfa50ee408395f
adamhutchings <adam.abahot@gmail.com>
2021-06-17 11:03:15 -0700

    Add mouse header

---
+++ src/input/mouse.hpp
@@ -0,0 +1,15 @@
+/**
+ * This provides a function to initialize the GLFW mouse callback, so that the
+ * camera can respond to mouse input.
+ */
+
+#pragma once
+
+namespace input {
+
+/**
+ * This also locks the mouse so it's hidden.
+ */
+void initialize_mouse_callback();
+
+}
