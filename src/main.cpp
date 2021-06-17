784819727d6ad69896e2752cb84aec67fc255f59
adamhutchings <adam.abahot@gmail.com>
2021-06-17 11:56:19 -0700

    Use keyboard callbacks

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include <chrono>
+#include <thread>
 #include <vector>
 
 #include <graphics/camera.hpp>
 #include <graphics/mesh.hpp>
 #include <graphics/shader.hpp>
 #include <graphics/texture.hpp>
+#include <input/keyboard.hpp>
 #include <input/mouse.hpp>
-
 #include <world/chunk.hpp>
 
 int main() {
@@ -27,6 +30,7 @@ int main() {
 	glfwShowWindow(wn);
 
 	input::initialize_mouse_callback(wn);
+	input::initialize_keyboard_callback(wn);
 
 	// Background sky color
 	glClearColor(0.4f, 0.7f, 1.0f, 1.0f);
@@ -45,12 +49,20 @@ int main() {
 	global_camera.yaw(210.0f, ViewMode::SET);
 
 	while (!glfwWindowShouldClose(wn)) {
+
+		auto start_of_frame = std::chrono::steady_clock::now();
+
+		input::do_movement();
+
 		// Update camera position
 		GAME_SHADER->set_uniform_value("game_matrix", global_camera.get_transform_matrix());
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		chunk.render();
 		glfwSwapBuffers(wn);
 		glfwPollEvents();
+
+		std::this_thread::sleep_until(start_of_frame + std::chrono::milliseconds(20));
+
 	}
 
 	tex::unload();
