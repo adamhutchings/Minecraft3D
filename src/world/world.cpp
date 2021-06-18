fabeb7ed01992b6faa77b7f1b4e19f553afa452b
adamhutchings <adam.abahot@gmail.com>
2021-06-18 14:02:59 -0700

    Create chunks using coordinates

--- src/world/world.cpp
+++ src/world/world.cpp
@@ -5,7 +5,7 @@ World::World() {
 	for (int x = 0; x < WORLD_WIDTH; ++x) {
 		for (int y = 0; y < WORLD_HEIGHT; ++y) {
 			for (int z = 0; z < WORLD_WIDTH; ++z) {
-				at(x, y, z) = Chunk(); // TODO
+				at(x, y, z) = Chunk(x, y, z);
 			}
 		}
 	}
