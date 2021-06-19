9e2a05439223994d935dd505e10f935ce4d2b351
adamhutchings <adam.abahot@gmail.com>
2021-06-18 17:30:45 -0700

    Fix 'AIR' to 'AIR_BLOCK'

--- src/world/gen/generator.cpp
+++ src/world/gen/generator.cpp
@@ -10,7 +10,7 @@ BlockType WorldGenerator::get_block_at(int x, int y, int z) {
 	// This is the level of the grass at a given coordinate.
 	int cutoff = noise.accumulatedOctaveNoise2D_0_1((float) x / 64, (float) z / 64, 2) * 32 + 32;
 
-	if (y > cutoff)     return AIR;
+	if (y > cutoff)     return AIR_BLOCK;
 	if (y == cutoff)    return GRASS_BLOCK;
 	if (y > cutoff - 4) return DIRT_BLOCK;
 	return STONE_BLOCK;
