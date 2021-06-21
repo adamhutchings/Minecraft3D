ed6bca17c0750af92ef8d439285eb8bdfccdb98a
adamhutchings <adam.abahot@gmail.com>
2021-06-21 13:07:26 -0700

    Add player header

---
+++ src/player/player.hpp
@@ -0,0 +1,15 @@
+/**
+ * The player, who can move, look, break blocks, etc.
+ */
+
+#pragma once
+
+#include <graphics/camera.hpp>
+
+class Player {
+
+public:
+	Camera camera;
+	Player(); // initialize the camera
+
+};
